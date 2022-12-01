9cd7d00b6e5288b04a00e6c53ae2fc6f12da71b5
Spec163 <45912087+Spec163@users.noreply.github.com>
2022-12-01 10:58:18 +0400

    initial commit

---
+++ src/test/java/edu/tltsu/medical_app/medical_app/MedicalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tltsu.medical_app.medical_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
